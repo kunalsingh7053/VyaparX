f6a697aad818c6174f6536bf71da8eac664fa09f
kunalsinghpatel <kunalsingh7053patel@gmail.com>
2026-01-23 00:02:52 +0530

    working on ai-buddy-service

---
+++ ai-buddy/src/sockets/socket.server.js
@@ -0,0 +1,34 @@
+const {Server} = require("socket.io");
+const jwt = require("jsonwebtoken");
+const cookie = require("cookie");
+
+async function initSocketServer(httpServer){
+
+const io = new Server(httpServer, {})
+
+io.use((socket,next)=>{
+const cookies = socket.handshake.headers?.cookie;
+const {token} = cookies ? cookie.parse(cookies) : {};
+if(!token){
+return next(new Error("Authentication error"));
+
+}
+try {
+    const decoded = jwt.verify(token,process.env.JWT_SECRET);
+    socket.user = decoded;
+    next();
+} catch (error) {
+    next(new Error("Authentication error"));
+}
+
+})
+
+io.on("connection",(socket)=>{
+console.log("New client connected",socket.id);
+
+
+
+})
+}
+
+module.exports = initSocketServer;
